b2ecd71636283d539fb0f358e78fc6586e986eae
muratayusuke <info@muratayusuke.com>
2016-09-23 15:25:12 +0900

    Bugfix

--- mac/setup.sh
+++ mac/setup.sh
@@ -32,7 +32,7 @@ brew install node || true
 brew install npm || true
 brew install --cocoa --with-gnutls -srgb emacs
 brew install carthage
-brew install global --with-exuberant-ctag
+brew install global --with-exuberant-ctags
 brew linkapps
 
 # nokogiri
