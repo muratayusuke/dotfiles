a7dd9a26dfe4bd6e17f60aa47e75f335f0022599
Yusuke Murata <info@muratayusuke.com>
2021-04-22 14:40:27 +0900

    Add rbenv rehash

--- mac/setup.sh
+++ mac/setup.sh
@@ -92,6 +92,7 @@ eval "$(rbenv init -)"
 LATEST_RUBY_VERSION=$(rbenv install -l | grep -v - | tail -1)
 rbenv install $LATEST_RUBY_VERSION
 rbenv global $LATEST_RUBY_VERSION
+rbenv rehash
 gem i bundler
 
 # python3
