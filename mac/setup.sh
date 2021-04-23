ebc61ccab4198c95707ae86307181169a3697152
Yusuke Murata <info@muratayusuke.com>
2021-04-23 10:34:05 +0900

    Install nodenv plugins

--- mac/setup.sh
+++ mac/setup.sh
@@ -43,6 +43,8 @@ brew install htop
 
 # node
 git clone git://github.com/nodenv/nodenv.git ~/.nodenv
+git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
+git clone https://github.com/nodenv/node-build-update-defs.git ~/.nodenv/plugins/node-build-update-defs
 
 # .dmg
 brew install google-chrome
